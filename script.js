372cd61f02191d799a1b92ac776a3d0f96ed670c
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2025-07-10 14:09:01 -0400

    wrote logic for computer choice w/ explanations

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Function for computer choice
-
 function getComputerChoice() {
   const computerChoice = Math.random(); //generates random number between 0 - 1
 
@@ -11,8 +10,11 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
-
 // Explanation:
 // Math.random() returns a floating-point, random number between 0 - 1.
 // The range of Math.random() is then divided into 3 equal parts (for rock, paper, scissors)
-// If computerChoice
+// If computerChoice is less than 1/3, "Rock" is returned.
+// If computerChoice is less than 2/3, but not less than 1/3, "Paper" is returned.
+// If computerChoice is greater than 2/3, (meaning, it does not fall in the categories of the first two) "Scissors" gets returned.
+
+//Function for Human Choice
